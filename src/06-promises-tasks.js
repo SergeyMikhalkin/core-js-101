73fe0a1f1650f71991cd510726022d69742d605b
sergeymikhalkin <sergey.mikhalkin.work@gmail.com>
2022-07-26 23:19:41 +0300

    feat: implement willYouMarryMe function in 06-promises-tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,12 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (isPositiveAnswer === undefined || typeof isPositiveAnswer !== 'boolean' || (Boolean(isPositiveAnswer) === false && isPositiveAnswer !== false)) reject(new Error('Wrong parameter is passed! Ask her again.'));
+    if (isPositiveAnswer) resolve('Hooray!!! She said "Yes"!');
+    else resolve('Oh no, she said "No".');
+  });
 }
 
 
