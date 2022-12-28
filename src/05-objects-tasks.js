0fca593ba08cbe88577a675d56716102dc5dc7a0
Sergey.Mikhalkin <sergey.mikhalkin.work@gmail.com>
2022-12-28 22:34:47 +0300

    feat: implement fromJSON function in 05-objects-tasks.js

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -56,8 +56,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = Object.create(proto);
+  const jsonObj = JSON.parse(json);
+  return Object.assign(obj, jsonObj);
 }
 
 
