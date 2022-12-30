d2973964303835d3b6897e0f90263ccd3ed784d9
Sergey.Mikhalkin <sergey.mikhalkin.work@gmail.com>
2022-12-30 16:00:43 +0300

    feat: implement getIdGeneratorFunction function in 08-functions-n-closures-tasks.js

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -185,8 +185,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let cache = startFrom - 1;
+  return () => {
+    cache += 1;
+    return cache;
+  };
 }
 
 
